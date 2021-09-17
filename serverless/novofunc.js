909b2c49a2e6c4ba567b0f42df1f2195e8972898
Gustavo Amorim <gamorim@informa.com.br>
2021-09-17 09:40:16 -0300

    serverless setup

---
+++ serverless/novofunc.js
@@ -0,0 +1,52 @@
+const http = require("http");
+
+module.exports.new = async (event) => {
+  try {
+    return httprequest().then((data) => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+      return response;
+    });
+  } catch (err) {
+    const message = err.message;
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ error: message }),
+    };
+    return response;
+  }
+};
+
+function httprequest() {
+  return new Promise((resolve, reject) => {
+    const options = {
+      host: "3.129.218.243",
+      path: "/funcionarios/list",
+      port: 3001,
+      method: "GET",
+    };
+    const req = http.request(options, (res) => {
+      if (res.statusCode < 200 || res.statusCode >= 300) {
+        return reject(new Error("statusCode=" + res.statusCode));
+      }
+      let body = [];
+      res.on("data", function (chunk) {
+        body.push(chunk);
+      });
+      res.on("end", function () {
+        try {
+          body = JSON.parse(Buffer.concat(body).toString());
+        } catch (e) {
+          reject(e);
+        }
+        resolve(body);
+      });
+    });
+    req.on("error", (e) => {
+      reject(e.message);
+    });
+    req.end();
+  });
+}
